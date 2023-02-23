b04ee0893968f27a9df92cac339705b0e37921be
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2023-02-23 08:21:46 -0600

    Fix

--- docker.sh
+++ docker.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-docker run --rm -i -e USER_ID=$(id -u) -e GROUP_ID=$(id -g) -v "$(pwd):/home/builder/:rw,z" -v "${HOME}/.ccache:/srv/ccache:rw,z" fr3akyphantom/droid-builder cd /home/builder/ && source build/envsetup.sh && lunch omni_REL-eng && make -j$(nproc --all) recoveryimage
+cd /home/runner
+
+docker run --rm -i -e USER_ID=$(id -u) -e GROUP_ID=$(id -g) -v "$(pwd):/home/builder/:rw,z" -v "${HOME}/.ccache:/srv/ccache:rw,z" fr3akyphantom/droid-builder source build/envsetup.sh && lunch omni_REL-eng && make -j$(nproc --all) recoveryimage
 export version=$(cat bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d '"' -f2)
 cp ~/out/target/product/REL/recovery.img ~/twrp/TWRP-$version-REL-$(date +"%Y%m%d")-Unofficial.img
