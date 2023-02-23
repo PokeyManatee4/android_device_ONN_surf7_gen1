0ebaee87f2bf3a5b3acf4a28713928c2bf181e1d
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2023-02-22 18:05:22 -0600

    Update permissive.sh

--- recovery/root/sbin/permissive.sh
+++ recovery/root/sbin/permissive.sh
@@ -4,5 +4,5 @@ setenforce 0
 
 # Get your device's block path where "system", "recovery", etc. lives.
 # That can be "/dev/block/bootdevice/by-name" or something like that.
-mkdir -p /dev/block/platform/mtk-msdc.0/by-name/
-busybox mount -o bind /dev/block/platform/mtk-msdc.0/11230000.msdc0/by-name/ /dev/block/platform/mtk-msdc.0/by-name/
+mkdir -p /dev/block/platform/by-name/
+busybox mount -o bind /dev/block/platform/by-name/ /dev/block/platform/mtk-msdc.0/by-name/
