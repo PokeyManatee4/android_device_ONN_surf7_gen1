a95185af890e9ef18e8f8d4809a9e1dfb4e04c18
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2023-02-24 16:26:13 -0600

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-docker run --rm -i -e USER_ID=$(id -u) -e GROUP_ID=$(id -g) -v "$(pwd):/home/builder/twrp/:rw,z" -v "${HOME}/.ccache:/srv/ccache:rw,z" fr3akyphantom/droid-builder bash
-
-cd /home/builder/twrp/
-source build/envsetup.sh
-lunch omni_ONN-eng
-make -j$(nproc --all) recoveryimage
+docker run --rm -i -e USER_ID=$(id -u) -e GROUP_ID=$(id -g) -v "$(pwd):/home/builder/twrp/:rw,z" -v "${HOME}/.ccache:/srv/ccache:rw,z" fr3akyphantom/droid-builder cd /home/builder/twrp/ && source build/envsetup.sh && lunch omni_ONN-eng && make -j$(nproc --all) recoveryimage
 exit
