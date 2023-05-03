03e4bc719af4fa6d53b3d72365f1e85c2dc84c9e
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2023-05-03 13:45:51 +0000

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /home/builder/tw/home/builder/twrp/ && source build/envsetup.sh && lunch omni_ONN-eng && make -j$(nproc --all) recoveryimage
+cd /home/builder/twrp && source build/envsetup.sh && lunch omni_ONN-eng && make -j$(nproc --all) recoveryimage
 
