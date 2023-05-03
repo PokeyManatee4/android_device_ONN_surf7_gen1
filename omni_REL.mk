9e95fc6a926a80a43ccf2fd14c3dd4e075e95172
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2023-05-03 19:16:41 +0000

    Update omni_REL.mk

--- omni_REL.mk
+++ omni_REL.mk
@@ -1,9 +1,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
-# Add this line if your device is 64-bit
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Otherwise, If you have 32-bit device, add the below line instead of above line
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
 
 # Another common config inclusion
 $(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
@@ -26,7 +24,7 @@ PRODUCT_MANUFACTURER := ONN
 # Forcefully add mtp support (adb is already there)
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp
-
+ 
 # Add fingerprint from Stock ROM build.prop
 PRODUCT_BUILD_PROP_OVERRIDES += \
     # These lines are from my device. You MUST Replace yours.
