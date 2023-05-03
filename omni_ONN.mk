bb008ddb500b5f7093dc6f479a9687ebbde17e38
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2023-05-03 11:26:16 -0400

    Create omni_ONN.mk

---
+++ omni_ONN.mk
@@ -0,0 +1,27 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+# Add this line if your device is 64-bit
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+# Otherwise, If you have 32-bit device, add the below line instead of above line
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk
+# Another common config inclusion
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+# If you are building from OmniROM's minimal source, Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+# Replace ${_CODENAME_} with your Device Name's Value.
+# Replace ${_VENDORNAME_} with your Brand's / Manufacturer's Value.
+PRODUCT_COPY_FILES += device/ONN/REL/prebuilt/kernel:kernel
+# Fles under $(LOCAL_PATH)/recovery/root/ gets automatically copied into recovery
+# PRODUCT_COPY_FILES += $(LOCAL_PATH)/recovery/root/*:root/*
+PRODUCT_DEVICE := REL
+PRODUCT_NAME := omni_REL
+PRODUCT_BRAND := ONN
+PRODUCT_MODEL := REL
+PRODUCT_MANUFACTURER := ONN
+# Forcefully add mtp support (adb is already there)
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
+# Add fingerprint from Stock ROM build.prop
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    # These lines are from my device. You MUST Replace yours.
+    BUILD_FINGERPRINT="ONN/100005206/mid7015_mk_32:9/PPR1.180610.011/kanghua10231432:user/release-keys" \
+    PRIVATE_BUILD_DESC="full_mid7015_mk_32-user 9 PPR1.180610.011 eng.fuqian.20191023.144734 release-keys"
