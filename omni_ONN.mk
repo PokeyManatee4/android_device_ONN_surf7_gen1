11ee3efb356ec4c9732febf2e4d1eee50e61e05d
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2023-05-05 18:55:10 +0000

    Update omni_ONN.mk

--- omni_ONN.mk
+++ omni_ONN.mk
@@ -1,19 +1,22 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-# Add this line if your device is 64-bit
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
 # Otherwise, If you have 32-bit device, add the below line instead of above line
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
+
 # Another common config inclusion
 $(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+
 # If you are building from OmniROM's minimal source, Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
+
 # Replace ${_CODENAME_} with your Device Name's Value.
 # Replace ${_VENDORNAME_} with your Brand's / Manufacturer's Value.
 PRODUCT_COPY_FILES += device/ONN/REL/prebuilt/kernel:kernel
 # Fles under $(LOCAL_PATH)/recovery/root/ gets automatically copied into recovery
 # PRODUCT_COPY_FILES += $(LOCAL_PATH)/recovery/root/*:root/*
+
 PRODUCT_DEVICE := REL
-PRODUCT_NAME := omni_REL
+PRODUCT_NAME := omni_ONN
 PRODUCT_BRAND := ONN
 PRODUCT_MODEL := REL
 PRODUCT_MANUFACTURER := ONN
