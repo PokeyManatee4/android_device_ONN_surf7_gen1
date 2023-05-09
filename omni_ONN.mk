3899112d07c7d0480b28d593035df4ca4fea730d
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2023-05-09 15:46:55 +0000

    Update omni_ONN.mk

--- omni_ONN.mk
+++ omni_ONN.mk
@@ -6,7 +6,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
 
 $(call inherit-product, vendor/omni/config/common.mk)
 
-PRODUCT_COPY_FILES += $(SRC_TARGET_DIR)/device/ONN/REL/prebuilt/zImage
+PRODUCT_COPY_FILES += device/ONN/REL/prebuilt/zImage
 
 PRODUCT_DEVICE := REL
 PRODUCT_NAME := omni_ONN
@@ -15,6 +15,7 @@ PRODUCT_MODEL := REL
 PRODUCT_MANUFACTURER := ONN
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="ONN/100005206/mid7015_mk_32:9/PPR1.180610.011/kanghua10231432:user/release-keys" \
     PRIVATE_BUILD_DESC="full_mid7015_mk_32-user 9 PPR1.180610.011 eng.fuqian.20191023.144734 release-keys"
