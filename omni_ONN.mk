eef20c131d18b35e0dfe720a016e92e42e6c1b5a
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2023-05-09 16:04:47 +0000

    Update omni_ONN.mk

--- omni_ONN.mk
+++ omni_ONN.mk
@@ -6,7 +6,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
 
 $(call inherit-product, vendor/omni/config/common.mk)
 
-PRODUCT_COPY_FILES += device/ONN/REL/prebuilt/kernel:kernel
+PRODUCT_COPY_FILES += device/ONN/REL/prebuilt/zImage:zImage
 
 PRODUCT_DEVICE := REL
 PRODUCT_NAME := omni_ONN
