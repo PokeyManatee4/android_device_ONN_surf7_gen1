947b09685127cd27370fd104f3601d6fc9d7bd86
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2023-05-16 19:03:39 +0000

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,7 +4,6 @@ TARGET_BOARD_PLATFORM := mt8167
 TARGET_NO_BOOTLOADER := true
 TARGET_BOOTLOADER_BOARD_NAME := mid7015_mk_32
 
-# These two are for MTK Chipsets only
 BOARD_USES_MTK_HARDWARE := true
 BOARD_HAS_MTK_HARDWARE := true
 
@@ -12,50 +11,45 @@ ALLOW_MISSING_DEPENDENCIES := true
 
 TARGET_USES_64_BIT_BINDER := true
 
-
 OVERRIDE_TARGET_FLATTEN_APEX := true
 
-# Recovery
 TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true      # To add info about F2FS Filesystem Data Block
-# Put The Size of your Recovery Partition below, get it from your "MT****_Android_scatter.txt"
+TARGET_USERIMAGES_USE_F2FS := true
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 1000000
-BOARD_FLASH_BLOCK_SIZE := 0                   # Might be different for your chip
-BOARD_HAS_NO_REAL_SDCARD := true              # Depricated
+BOARD_FLASH_BLOCK_SIZE := 0
+BOARD_HAS_NO_REAL_SDCARD := true
 BOARD_SUPPRESS_SECURE_ERASE := true
-BOARD_HAS_NO_MISC_PARTITION := true           # Delete if your partition table has /misc
+BOARD_HAS_NO_MISC_PARTITION := true 
 BOARD_RECOVERY_SWIPE := true
 BOARD_USES_MMCUTILS := true
 BOARD_SUPPRESS_EMMC_WIPE := true
-BOARD_CHARGER_SHOW_PERCENTAGE := true
+BOARD_CHARGER_SHOW_PERCENTAGE := false
 RECOVERY_SDCARD_ON_DATA := true               # Optional: If /sdcard partition is emulated on /data partition 
 
 # TWRP stuff
-TW_EXCLUDE_SUPERSU := true                   # true/false: Add SuperSU or not
+TW_EXCLUDE_SUPERSU := false                  # true/false: Add SuperSU or not
 TW_INCLUDE_CRYPTO := true                     # true/false: Add Data Encryption Support or not
 TW_INPUT_BLACKLIST := "hbtp_vm"               # Optional: Disables virtual mouse
-TW_SCREEN_BLANK_ON_BOOT := false
+TW_SCREEN_BLANK_ON_BOOT := true
 TW_USE_TOOLBOX := true
 TW_THEME := portrait_hdpi                    # Set the exact theme you wanna use. If resulation doesn't match, define the height/width
 DEVICE_RESOLUTION := 1024x600                # The Resolution of your Device
 TARGET_SCREEN_HEIGHT := 1024                    # The height
 TARGET_SCREEN_WIDTH := 600                      # The width
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
-# Set the Brightness Control File Path below (as per your chip/device)
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_SECONDARY_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt8167/leds/lcd-backlight/brightness
-# Set the Path of Logical Units (LUNs) for Storage below (as per your chip/device)
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0.auto/gadget/lun%d/file
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun/file
 TW_MAX_BRIGHTNESS := 255
 TW_DEFAULT_BRIGHTNESS := 80                   # Set custom brightness, low is better
 
 TW_INCLUDE_NTFS_3G := true                   # Include NTFS Filesystem Support
-TW_INCLUDE_FUSE_EXFAT := false              # Include Fuse-ExFAT Filesystem Support
-TWRP_INCLUDE_LOGCAT := true                   # Include LogCat Binary
+TW_INCLUDE_FUSE_EXFAT := true              # Include Fuse-ExFAT Filesystem Support
+TWRP_INCLUDE_LOGCAT := false                   # Include LogCat Binary
 TW_INCLUDE_FB2PNG := false                     # Include Screenshot Support
 TW_DEFAULT_LANGUAGE := en                     # Set Default Language 
-TW_EXTRA_LANGUAGES := true
+TW_EXTRA_LANGUAGES := false
 
 # Kernel
 TARGET_SCREEN_DENSITY := 160
@@ -68,9 +62,9 @@ BOARD_KERNEL_TAGS_OFFSET := 0x14000000
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
-BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_SEPARATED_DTBO := true
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/zImage:kernel
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/zImage:zImage
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/zImage-dtbo
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/root/etc/recovery.fstab
 
