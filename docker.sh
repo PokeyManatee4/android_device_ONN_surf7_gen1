0880dd11ec4dac4e220e1a00c010de404e6de734
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2023-05-18 15:12:14 +0000

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 echo "Starting"
+repo init -u https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-9.0
+repo sync
 cd ~/twrp
 echo "Sourcing"
 source ~/twrp/build/envsetup.sh
 echo "Lunching"
+export ALLOW_MISSING_DEPENDENCIES=true;
 lunch omni_ONN-eng
 echo "Making"
 mka recoveryimage
