719b150e30fb63b9d2a428d92b7f8af1c5b45694
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2023-05-19 20:13:32 +0000

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -3,7 +3,7 @@
 echo "Starting"
 cd /home/runner/twrp
 echo "Sourcing"
-. /home/runner/twrp/build/envsetup.sh
+source /home/runner/twrp/build/envsetup.sh
 echo "Lunching"
 export ALLOW_MISSING_DEPENDENCIES=true
 lunch omni_ONN-eng
