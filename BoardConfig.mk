06b1a47c517a97190a5cf161dd8b1da09664626d
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2023-05-23 14:55:10 +0000

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,8 +50,9 @@ BOARD_PREBUILT_DTBOIMAGE := $(LOCAL_PATH)/prebuilt/zImage-dtbo
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_CMDLINE :=  bootopt=64S3,32N2,32N2 buildvariant=user veritykeyid=id:7e4333f9bba00adfe0ede979e28ed1920492b40f
 BOARD_KERNEL_BASE := 0x40000000
+BOARD_HEADER_VERSION := 1
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x15000000 --second_offset 0x00f00000 --tags_offset 0x14000000 --header_version 1
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x15000000 --second_offset 0x00f00000 --tags_offset 0x14000000
 
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/root/etc/recovery.fstab
 
