82923c1f708f4a1fe03c8a92487346e55f5e0049
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2023-05-25 19:38:56 -0400

    Update and rename Makefile to config.sh

--- config.sh
+++ config.sh
@@ -4,7 +4,7 @@ TWRP_SOURCE3 = https://github.com/PhantomZone54/twrp_sources_norepo/releases/dow
 TWRP_SOURCE4 = https://github.com/PhantomZone54/twrp_sources_norepo/releases/download/v3.4.0-20201103/MinimalOmniRecovery-twrp-9.0-norepo-20201103.tzst.ad
 ROOM_SOURCE = https://gist.github.com/rokibhasansagar/247ddd4ef00dcc9d3340397322051e6a/raw/roomservice_90.py
 
-config:
+
 	 mkdir ~/twrp/
 	 wget -q --show-progress --progress=bar:force "$(TWRP_SOURCE1)" "$(TWRP_SOURCE2)" "$(TWRP_SOURCE3)" "$(TWRP_SOURCE4)"
          tar --zstd -xf MinimalOmniRecovery-twrp-* -directory /home/runner/twrp && rm MinimalOmniRecovery*
