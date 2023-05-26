c3b60bda764b7724f69de9f48c4df329163ecae9
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2023-05-26 10:43:05 -0400

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -75,7 +75,8 @@ PLATFORM_VERSION := 16.1.0
 
 # TWRP Configuration
 TW_THEME := portrait_mdpi
-TW_EXTRA_LANGUAGES := true
-TW_SCREEN_BLANK_ON_BOOT := true
+TW_EXTRA_LANGUAGES := false
+TW_SCREEN_BLANK_ON_BOOT := false
+
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
