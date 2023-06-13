0aa76ff7fd29cbbcc8aab807bb076e54eb473c59
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2023-06-13 18:08:08 -0500

    Delete recovery/root/sbin directory

--- recovery/root/sbin/permissive.sh
+++
@@ -1,8 +0,0 @@
-#!/sbin/sh
-
-setenforce 0
-
-# Get your device's block path where "system", "recovery", etc. lives.
-# That can be "/dev/block/bootdevice/by-name" or something like that.
-mkdir -p /dev/block/platform/by-name/
-busybox mount -o bind /dev/block/platform/by-name/ /dev/block/platform/mtk-msdc.0/by-name/
